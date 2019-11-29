78769c41e87e2789b13ceff8b668adbe0905a67a
calcgen2 <raj.shrestha778@gmail.com>
2019-11-29 11:48:33 +0545

    Start fingerpringting from last stop.

--- example.py
+++ example.py
@@ -8,6 +8,7 @@ import warnings
 import json
 import time
 import csv
+import pandas as pd
 warnings.filterwarnings("ignore")
 
 
@@ -25,10 +26,23 @@ songs_arr = client.list_objects(Bucket='songs-1')['Contents']
 # pop out the dir name
 songs_arr.pop(0)
 num_vcpu = 4
+
+# delete previous os dir
 dir_name = 'fopi_songs'
 if os.path.isdir(dir_name):
     shutil.rmtree(dir_name)
 
+# read from log
+last_run_at = 0
+if os.path.isfile('log.csv'):
+    df = pd.read_csv('log.csv', header=None)
+    last_run_at = int(df.tail(1)[0])
+
+# update song_arr
+# start_from = last_run_at*num_vcpu - 1
+# song_arr = song_arr[start_from:]
+
+# load config for dejavu db
 with open("dejavu.cnf") as f:
     config = json.load(f)
 
@@ -38,34 +52,35 @@ if __name__ == '__main__':
         # create a Dejavu instance
     djv = Dejavu(config)
     os.mkdir(dir_name)
-    
+
     # Fingerprint all the mp3's in the directory we give it
-    songs_tag=[]
-    starting_time=time.time()
-    print ("start time: ",starting_time)
-    for num, song in enumerate(songs_arr, 1):    
+    songs_tag = []
+    starting_time = time.time()
+    print("start time: ", starting_time)
+    for num, song in enumerate(songs_arr, 1):
         client.download_file(Bucket='songs-1',
-                            Key=song['Key'],
-                            Filename=song['Key'])
+                             Key=song['Key'],
+                             Filename=song['Key'])
         songs_tag.append(song['Key'])
         if ((num) == len(songs_arr)) or (((num) % num_vcpu) == 0):
-            start=time.time()
+            start = time.time()
             djv.fingerprint_directory(dir_name, [".mp3"])
-            print("batch:",songs_tag,time.time()-start)
-            with open('log.csv', 'a') as writeFile:
-               writer= csv.writer(writeFile)
-               writer.writerows([[num/4,songs_tag,time.time()-start]])
-            songs_tag=[]
+            print("batch:", songs_tag, time.time()-start)
+            if num > last_run_at:
+                with open('log.csv', 'a') as writeFile:
+                    writer = csv.writer(writeFile)
+                    writer.writerows([[num/4, songs_tag, time.time()-start]])
+            songs_tag = []
             shutil.rmtree(dir_name)
             os.mkdir(dir_name)
-    final_end_time=time.time()
+    final_end_time = time.time()
     print("end time: ", final_end_time)
     with open('total_time.csv', 'a') as writeFile:
         writer = csv.writer(writeFile)
-        writer.writerows([["complete time",final_end_time-starting_time]])
+        writer.writerows([["complete time", final_end_time-starting_time]])
 
-            # load config from a JSON file (or anything outputting a python dictionary)
-    
+        # load config from a JSON file (or anything outputting a python dictionary)
+
     # Recognize audio from a file
     # song = djv.recognize(
     #    FileRecognizer, "mp3/Sean-Fournier--Falling-For-You.mp3")
